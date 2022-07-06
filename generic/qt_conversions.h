abbd59e9fbc073456935e515e1a1b8f4133b4754
al <azrael999@posteo.de>
2022-07-07 01:32:19 +0200

    the clang format raider rides again

--- generic/qt_conversions.h
+++ generic/qt_conversions.h
@@ -4,28 +4,28 @@
 
 #include <generic/types.h>
 
-
 template<typename T>
-QString qt_conv(const T& input)
+QString
+qt_conv(const T& input)
 {
-    return QString::fromStdString(input);
+  return QString::fromStdString(input);
 }
 
 template<typename T>
-requires std::is_integral<T>::value
-QString qt_conv(const T& input)
+requires std::is_integral<T>::value QString
+qt_conv(const T& input)
 {
-    return QString::number(input);
+  return QString::number(input);
 }
 
 template<typename T>
-QStringList qt_conv(const vector<T>& vec)
+QStringList
+qt_conv(const vector<T>& vec)
 {
-    QStringList qlist;
+  QStringList qlist;
 
-    for(const auto& val : vec)
-    {
-        qlist << qt_conv(val);
-    }
-    return qlist;
+  for (const auto& val : vec) {
+    qlist << qt_conv(val);
+  }
+  return qlist;
 }
