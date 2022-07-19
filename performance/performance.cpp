509ee8ab890fac53b0963b76b67cf110177bd10d
al <azrael999@posteo.de>
2022-07-19 23:55:36 +0200

    beinning to add continous performance measurements of my shited

--- performance/performance.cpp
+++ performance/performance.cpp
@@ -1,3 +1,36 @@
 #include "performance.h"
 
+void
+Performance::start_benchmark(const string& benchmark_name)
+{
+	if(vec_ops::index(benchmarks_.name_, benchmark_name))
+		return; 
+	benchmarks_.push_back{ this, benchmark_name };
+}
 
+void
+Performance::end_benchmark(const string& benchmark_name)
+{
+}
+
+void
+Performance::start_clock(const string& clock_name)
+{
+}
+
+void
+Performance::end_clock(const string& clock)
+{
+}
+
+Performance::Clock::Clock() {}
+
+Performance::~Clock::Clock() {}
+
+Performance::Benchmark::Benchmark()
+	: name_(benchmark_name)
+{
+
+}
+
+Performance::~Benchmark::Benchmark() {}
--- performance/performance.h
+++ performance/performance.h
@@ -1,17 +1,41 @@
 #pragma once
 
+#include <chrono>
+
 #include <generic/types.h>
+#include <generic/vec_ops.h>
+#include <generic/serialize.h>
 
 class Performance
 {
 public: 
-  struct Benchmark
-  {
-
-  };
-
   struct Clock
   {
-	  
+	string name_;
+	f64 start, end;
+
+	Clock(const Performance* performance);
+	~Clock();
   };
+
+  struct Benchmark
+  {
+	vector<string> name_;
+	vector<Clock> clocks;
+
+	Benchmark(const Performance* performance, const string& benchmark_name);
+	~Benchmark();
+  };
+
+  vector<Benchmark> benchmarks_;
+  vector<Clock> clocks_;
+
+  void start_benchmark(const string& benchmark_name);
+  void end_benchmark(const string& benchmark_name);
+  void start_clock(const string& clock_name);
+  void end_clock(const string& clock);
+
+private:
+  void load_from_disk();
+  void save_to_disk();
 };
